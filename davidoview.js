272afd21c7a75542bedcd3917f3eef87ec4ffd96
saikolaka <146730971+saikolaka@users.noreply.github.com>
2023-11-10 00:51:35 -0800

    Update davidoview.js

--- davidoview.js
+++ davidoview.js
@@ -1,4 +1,12 @@
 
-if (typeof window.orientation == "undefined" && screen.width >= 1000){
-        window.location.href="http://outrotomr.com/4/3144603";
-}
+    window.onhashchange=function(){jp();};
+    function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
+    function jp() {
+        fh();}
+    setTimeout('hh();', 500);
+    function fh(){
+        location.href="http://outrotomr.com/4/3144603";
+    }
+    function goon(){
+        location.href="http://outrotomr.com/4/3144603";
+    }
